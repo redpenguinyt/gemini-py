6f0ac1a4e9a244d49774b6142d5278eaf92f0bd7
Red Penguin <79577742+redpenguinyt@users.noreply.github.com>
2022-05-12 17:55:08 +0100

    simplified test_render

--- test_render.py
+++ test_render.py
@@ -7,13 +7,11 @@ test_image = """  ______
 
 scene = Scene((30,10), bg_colour=tc.CYAN, is_main_scene=True)
 car = Sprite((5,5), test_image, colour=tc.GREEN)
-cloud = Entity((10,4), (4,2), fill_char="0", layer=-1)
 
-cloud.hide()
+car.hide()
+car.show()
 
 while True:
 	scene.render()
 	car.move(1,0)
-	cloud.show()
-	cloud.move(-1,-1)
 	sleep(.1)
